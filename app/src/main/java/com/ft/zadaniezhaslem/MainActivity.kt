af860a5dca857b99a24154bd1719d333199c5cf6
Dominos0321 <dsch792@gmail.com>
2023-02-01 13:11:13 +0100

    Komit drugi

--- app/src/main/java/com/ft/zadaniezhaslem/MainActivity.kt
+++ app/src/main/java/com/ft/zadaniezhaslem/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ft.zadaniezhaslem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var przycisk = findViewById<Button>(R.id.BT1)
+
+        przycisk.setOnClickListener {
+            if(findViewById<TextInputEditText>(R.id.TIET1).text.toString() == findViewById<TextInputEditText>(R.id.TIET2).text.toString()){
+                findViewById<CheckBox>(R.id.ChB5).isChecked = true
+            }
+
+        }
     }
 }
